aa76a58aae88506417f95c1c5e9992b0dc504e7f
Collin Dapper <collindapper@gmail.com>
2022-08-12 18:44:18 -0700

    Controller updates

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -13,5 +13,68 @@ module Api
 
       render 'api/properties/show', status: :ok
     end
+
+    def create
+      token = cookies.signed[:airbnb_session_token]
+      session = Session.find_by(token: token)
+      user = session.user
+      @property = user.properties.new(property_params)
+
+      if @property.save
+        render 'api/propertties/create'
+      end
+    end
+
+    def update
+      token = cookies.signed[:airbnb_session_token]
+      session = Session.find_by(token: token)
+      user = session.user
+
+      @property = Property.find_by(id: params[:id])
+
+      return render json: {message: 'not authorized'}, status: :forbidden if @property.user_id != user.id
+
+      return render 'not_found', status: :not_found if not @property
+      return render 'bad_request', status: :bad_request if not @property.update(property_params)
+
+      render 'api/properties/show, status: :ok'
+    end
+
+    def destroy
+      token = cookies.signed[:airbnb_session_token]
+      session = Session.find_by(token: token)
+
+      return render json: { success: false } unless session
+
+      user = session.user
+      property = Property.find_by(id: params[:id])
+
+      # if property and property.user == user and property.destroy
+      if property and property.destroy
+        render json: {
+          success: true
+        }
+      else
+        render json: {
+          success: false
+        }
+      end
+    end
+
+    def index_by_user
+      user = User.find_by(username: params[:username])
+
+      if user
+        @properties = user.properties
+        render 'api/properties/details'
+      end
+    end
+
+    private
+
+    def property_params
+      params.require(:property).permit(:title, :description, :city, :country, :property_type, :price_per_night, :max_guests, :bedrooms, :beds, :baths, :user, :image)
+    end
+    
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,38 @@ class StaticPagesController < ApplicationController
   def login
     render 'login'
   end
+
+  def booking_success
+    @data = { booking_id: params[:id] }.to_json
+    render 'booking_success'
+  end
+
+  def bookings
+    @data = { username: params[:username] }.to_json
+    render 'bookings'
+  end
+
+  def listings
+    @data = { username: params[:username] }.to_json
+    render 'listings'
+  end
+
+  def add_property
+    render 'add_property'
+  end
+
+  def edit_property
+    @data = { property_id: params[:id] }.to_json
+    render 'edit_property'
+  end
+
+  def property_reservations
+    @data = { username: params[:username] }.to_json
+    render 'property_reservations'
+  end
+
+  def reservations
+    @data = { username: params[:username] }.to_json
+    render 'reservations'
+  end
 end
