f9ca6e0309b065d5634a346586076573838bd387
Collin Dapper <collindapper@gmail.com>
2022-08-30 18:55:48 -0700

    Stripe fix

--- app/javascript/src/property/bookingWidget.jsx
+++ app/javascript/src/property/bookingWidget.jsx
@@ -79,7 +79,7 @@ class BookingWidget extends React.Component {
       .then(handleErrors)
       .then(response => {
         const stripe = Stripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
-  
+        console.log('response', response)
         stripe.redirectToCheckout({
           // Make the id field from the Checkout Session creation API response
           // available to this file, so you can provide it as parameter here
@@ -89,10 +89,11 @@ class BookingWidget extends React.Component {
           // If `redirectToCheckout` fails due to a browser or network
           // error, display the localized error message to your customer
           // using `result.error.message`.
+          console.log('result', result)
         });
       })
       .catch(error => {
-        console.log(error);
+        console.log('error', error);
       })
   }
 
